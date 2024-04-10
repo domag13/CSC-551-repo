afaf295a67b167c646c7b953399c5da2b1896ce9
dom14 galuppo <domanlupo@gamail.com>
2024-04-10 11:56:52 -0500

    changed models.py

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -48,20 +48,16 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     address = models.CharField(max_length=250)
+    adress_2 = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
+    state = models.CharField(max_lenght=2)
     phone = models.CharField(max_length=10)
 
     def __str__(self):
         return f'Customer {self.id}'
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
