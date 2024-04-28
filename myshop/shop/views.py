770aaf69a3320116736c6e02af207c8bf39428e2
sdr94625 <sdr94625@creighton.edu>
2024-04-28 15:38:32 -0500

    login works

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+from .forms import LoginForm
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 
@@ -26,4 +30,24 @@ def product_detail(request, id, slug):
         'shop/product/detail.html',
         {'product': product,
          'cart_product_form': cart_product_form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=['password'])
+
+            if user is not None:
+                if user.is_active():
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                form = LoginForm()
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'shop/login.html', {'form': form})
 
